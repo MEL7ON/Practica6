b31d13ca7a913deb2ebf9a67b925d4afc036b3ec
Gorevoy Ivanichkin Dzyura <is26gorevoysa@artcollege.ru is26dzyurava@artcollege.ru is26ivanichkinva@artcollege.ru>
2024-02-19 12:40:57 +0300

    Добавьте файлы проекта.

---
+++ Proect1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proect1
+{
+  internal class Programm
+   {
+    static void Main(string[] args)
+    {
+
+    }
+   }
+}
+
